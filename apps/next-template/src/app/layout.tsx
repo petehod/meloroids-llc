89ae6d4c88668e84ded339ae620bde8c56b3d0cf
petehod <pete@secretlab.com>
2024-12-21 07:29:01 -0600

    add boilerplate next app

---
+++ apps/next-template/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "@repo/ui/styles.css";
+import { GA_ID } from "@repo/assets/googleAnalytics";
+const plus_jakarta_sans = Plus_Jakarta_Sans({ subsets: ["latin"] });
+import { GoogleAnalytics } from "@next/third-parties/google";
+
+export const metadata: Metadata = {
+  title: "Next Template",
+  description: "This is a starter template for next app"
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      {process.env.NODE_ENV === "production" && (
+        <GoogleAnalytics gaId={GA_ID} />
+      )}
+      <body
+        className={`${plus_jakarta_sans.className} flex items-center justify-center min-h-screen home gradient-bg p-2`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
