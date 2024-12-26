fe89f39f58779a30363f285dd09026fd50e08375
petehod <pete@secretlab.com>
2024-12-26 07:52:19 -0600

    add common package

--- apps/meloroids/src/app/page.tsx
+++ apps/meloroids/src/app/page.tsx
@@ -1,3 +1,5 @@
+import { UserSchema } from "@repo/common/user";
 export default function Home() {
+  UserSchema.parse({ id: "asd" });
   return <main>hello world</main>;
 }
---
+++ packages/common/src/validators/user.validator.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+export const UserSchema = z.object({
+  id: z.string()
+});
+
+export type User = z.infer<typeof UserSchema>;
