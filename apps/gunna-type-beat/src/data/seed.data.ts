31c634ff326b495637705a8761a5196328305ec2
petehod <pete@secretlab.com>
2024-12-31 08:23:20 -0600

    update seed data

--- apps/gunna-type-beat/src/data/seed.data.ts
+++ apps/gunna-type-beat/src/data/seed.data.ts
@@ -5,12 +5,12 @@ type SeedArtist = Artist & {
   imagePath: string;
 };
 
-export const GUNNA_ID = "d52ab875-267c-4c00-9b47-85140037122e";
-export const ONE_OF_WUN_ID = "a45554a0-88a1-427f-96f9-b866cc9ca157";
-export const RODDY_RICCH_ID = "198ace0d-1459-412d-b6fe-15c4dfef47a7";
-export const NORMANI_ID = "b3d24030-7dc1-4655-9838-7bf04048b780";
-export const LEON_BRIDGES_ID = "ffa1c848-9fcc-4613-b86d-64b550c86413";
-export const OFFSET_ID = "8968ad29-0149-4076-b0d6-c9bf873be5b8";
+export const GUNNA_ID = "ed06053c-bc23-4ad9-b623-a37a32884193";
+export const ONE_OF_WUN_ID = "1e674df5-106e-491a-85c0-f825242ec1fa";
+export const RODDY_RICCH_ID = "74953768-8d81-45fa-8a27-872398df8d87";
+export const NORMANI_ID = "3d4809c4-f85a-4c4f-86e4-1e402a4a3045";
+export const LEON_BRIDGES_ID = "0b5614ef-c469-41ab-8696-f701de05c29c";
+export const OFFSET_ID = "53bed06d-8c6c-4f93-a3a1-ca88a3b12e14";
 
 export const SEED_ARTISTS: SeedArtist[] = [
   {
